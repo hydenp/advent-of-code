fe8e02e958f209f84f1b371ac3a776107092fb1a
Hyden Polikoff <hyden.dev@gmail.com>
2022-12-01 18:55:04 -0700

    recursive solutions day 1

--- day_1/part1.py
+++ day_1/part1.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def part1_solution():
     max_elf_calories = 0
     with open('input.txt', 'r') as file:
@@ -13,5 +16,21 @@ def part1_solution():
     return max_elf_calories
 
 
+def part1_recursive_solution(max_elf_cals, current_elf_cals, lines, line_index):
+    if line_index == len(lines):
+        return max_elf_cals
+    if lines[line_index] == '\n':
+        if current_elf_cals > max_elf_cals:
+            return part1_recursive_solution(current_elf_cals, 0, lines, line_index+1)
+        else:
+            return part1_recursive_solution(max_elf_cals, 0, lines, line_index+1)
+    else:
+        return part1_recursive_solution(max_elf_cals, current_elf_cals+int(lines[line_index].strip('\n')), lines, line_index+1)
+
+
 if __name__ == '__main__':
     print(part1_solution())
+
+    sys.setrecursionlimit(2_500)
+    input_lines = open("input.txt").readlines()
+    print(part1_recursive_solution(0, 0, input_lines, 0))
--- day_1/part2.py
+++ day_1/part2.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def part2_solution():
     max_elf_calories = [0, 0, 0]
     with open('input.txt', 'r') as file:
@@ -19,5 +22,27 @@ def part2_solution():
     return sum(max_elf_calories)
 
 
+def part2_recursive_solution(max_elf_calories, current_elf_calories, lines, line_index):
+    if line_index == len(lines):
+        return sum(max_elf_calories)
+    if lines[line_index] == '\n':
+        if current_elf_calories > max_elf_calories[0]:
+            return part2_recursive_solution([current_elf_calories, *max_elf_calories[:2]], 0, lines, line_index + 1)
+        elif current_elf_calories > max_elf_calories[1]:
+            return part2_recursive_solution([max_elf_calories[0], current_elf_calories, max_elf_calories[1]], 0,
+                                            lines, line_index + 1)
+        elif current_elf_calories > max_elf_calories[2]:
+            return part2_recursive_solution([*max_elf_calories[:2], current_elf_calories], 0, lines, line_index + 1)
+        else:
+            return part2_recursive_solution(max_elf_calories, 0, lines, line_index + 1)
+    else:
+        return part2_recursive_solution(max_elf_calories, current_elf_calories + int(lines[line_index].strip('\n')),
+                                        lines, line_index + 1)
+
+
 if __name__ == '__main__':
     print(part2_solution())
+
+    sys.setrecursionlimit(2_500)
+    input_lines = open("input.txt").readlines()
+    print(part2_recursive_solution([0, 0, 0], 0, input_lines, 0))
